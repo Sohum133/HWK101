6e7725182589e8f7a63f40836bcdc7bf781cc68a
Sohum133 <99077044+Sohum133@users.noreply.github.com>
2024-03-08 16:48:52 +0000

    Add files via upload

---
+++ Project 101/roll_a_dice.py
@@ -0,0 +1,74 @@
+import random
+
+response = "y"
+
+response = input("Would you like to roll a dice?")
+
+while response == "y":
+    no = random.randint(1,6)
+
+if conditiput.on(no):
+    print(no)
+
+response1 = input("Would you like to continue? y/n")
+
+if response1 == y:
+    response = response1
+else:
+    
+
+
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+
+    if no == 2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+        print("[-----]")
+
+    if no == 3:
+        print("[-----]")
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[-----]")
+
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+
+    if no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+
+quit()
+
+
+
+
+
+
+
+
+
+
+
